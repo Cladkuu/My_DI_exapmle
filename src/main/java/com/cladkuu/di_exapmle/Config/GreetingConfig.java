65e379cab844df2586f064a2d40063a2525f3111
Artem LapTopAccount <cladkuu52rus@yandex.ru>
2022-01-07 11:38:31 +0300

    Adding @Primary and @Profile in Java Config

--- src/main/java/com/cladkuu/di_exapmle/Config/GreetingConfig.java
+++ src/main/java/com/cladkuu/di_exapmle/Config/GreetingConfig.java
@@ -1,13 +1,25 @@
 package com.cladkuu.di_exapmle.Config;
 
-import com.cladkuu.di_exapmle.Service.ConstructorGreetingServiceImpl;
-import com.cladkuu.di_exapmle.Service.PropertyGreetingServiceImpl;
-import com.cladkuu.di_exapmle.Service.SetterGreetingServiceImpl;
+import com.cladkuu.di_exapmle.Service.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingConfig {
+
+    @Profile("ES")
+    @Bean
+    I18NSpanishGreetingServiceImpl i18NSpanishGreetingService(){
+        return new I18NSpanishGreetingServiceImpl();
+    }
+
+    @Profile({"default", "EN"})
+    @Bean("i18nService")
+    I18NEnglishGreetingServiceImpl i18NEnglishGreetingService(){
+        return new I18NEnglishGreetingServiceImpl();
+    }
 
     @Bean
     ConstructorGreetingServiceImpl constructorGreetingServiceImpl(){
@@ -22,5 +34,11 @@ public class GreetingConfig {
     @Bean
     SetterGreetingServiceImpl setterGreetingServiceImpl() {
         return new SetterGreetingServiceImpl();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingServiceImpl primaryGreetingServiceImpl(){
+        return new PrimaryGreetingServiceImpl();
     }
 }
--- src/main/java/com/cladkuu/di_exapmle/Service/I18NEnglishGreetingServiceImpl.java
+++ src/main/java/com/cladkuu/di_exapmle/Service/I18NEnglishGreetingServiceImpl.java
@@ -3,8 +3,8 @@ package com.cladkuu.di_exapmle.Service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile({"default", "EN"})
-@Service("i18nService")
+/*@Profile({"default", "EN"})
+@Service("i18nService")*/
 public class I18NEnglishGreetingServiceImpl implements GreetingService {
     @Override
     public String SayHello() {
